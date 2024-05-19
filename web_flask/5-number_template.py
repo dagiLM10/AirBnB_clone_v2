e262bede39ea78cc596ef0d9631b8004cbaf8915
dagiLM10 <dagiberhanu19@gmail.com>
2024-05-19 00:54:27 -0700

    update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -13,7 +13,7 @@ def index():
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb'>, strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
